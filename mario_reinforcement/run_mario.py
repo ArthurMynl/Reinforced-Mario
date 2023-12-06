0401c2d0b1a3763c53e6b59fb193cbd5ef69d53a
achamouleau <aurelie.chamouleau@reseau.eseo.fr>
2023-12-06 17:10:36 +0100

    first try learning

--- mario_reinforcement/run_mario.py
+++ mario_reinforcement/run_mario.py
@@ -1,5 +1,5 @@
-from nes_py.wrappers import JoypadSpace
 import gym_super_mario_bros
+from nes_py.wrappers import JoypadSpace
 from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
 import gym
 
@@ -7,13 +7,12 @@ env = gym.make('SuperMarioBros-v0', apply_api_compatibility=True, render_mode="h
 env = JoypadSpace(env, SIMPLE_MOVEMENT)
 
 done = True
-env.reset()
-for step in range(100000):
+env.reset() # reset environment
+for step in range(5000):
     action = env.action_space.sample()
-    obs, reward, terminated, truncated, info = env.step([5])
+    obs, reward, terminated, truncated, info = env.step(action)
     done = terminated or truncated
-
     if done:
-       state = env.reset()
+        state = env.reset()
 
 env.close()
